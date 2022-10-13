fea6dbd97f57b512dd80b47f5e7a5700788c97eb
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-10-13 19:24:02 +0900

    Update app.js

--- node/login-lecture/app.js
+++ node/login-lecture/app.js
@@ -1,17 +1,31 @@
-const express = require("express");
-const app = express();
-
-app.get("/",(req, res)=>{
- //기능
- res.send("여기는 루트입니다")
+const http = require("http");
+const app = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end("여기는 루트입니다");
+    }else if(req.url === '/login'){
+        res.end("여기는 login 화면 입니다");
+    }
 });
 
-app.get("/login",(req, res)=>{
-    //기능
-    res.send("여기는 login 화면입니다")
-   });
-
-
-app.listen(3000, function() {
-    console.log("서버 가동");
+app.listen(3001, () =>{
+    console.log("http로 가동된 서버입니다");
 });
+
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/",(req, res)=>{
+//  //기능
+//  res.send("여기는 루트입니다")
+// });
+
+// app.get("/login",(req, res)=>{
+//     //기능
+//     res.send("여기는 login 화면입니다")
+//    });
+
+
+// app.listen(3000, function() {
+//     console.log("서버 가동");
+// });
